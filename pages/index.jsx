7c761a74c0bcba9f163896f8bbe6b4958f08ecf3
Yankel Bensimhon <yankelbensimhon@MacBook-Air-de-Yankel.local>
2025-07-09 01:29:14 +0200

    fix: imports relatifs pour build Next.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import AccueilActions from "@/components/AccueilActions";
-import TimeLineRecouvrement from "@/components/TimeLineRecouvrement";
-import LegalModes from "@/components/LegalModes";
-import AvantagesAvocat from "@/components/AvantagesAvocat";
-import MiseEnDemeureGenerator from "@/components/MiseEnDemeureGenerator";
-import Footer from "@/components/Footer";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AccueilActions from "../components/AccueilActions";
+import TimeLineRecouvrement from "../components/TimeLineRecouvrement";
+import LegalModes from "../components/LegalModes";
+import AvantagesAvocat from "../components/AvantagesAvocat";
+import MiseEnDemeureGenerator from "../components/MiseEnDemeureGenerator";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
