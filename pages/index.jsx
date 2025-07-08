8754b3ff88b12b7e16df933a31c6af2cb91798f6
Yankel Bensimhon <yankelbensimhon@MacBook-Air-de-Yankel.local>
2025-07-09 01:20:11 +0200

    fix imports et index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,22 @@
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
-import TimelineRecouvrement from "@/components/TimeLineRecouvrement";
+import AccueilActions from "@/components/AccueilActions";
+import TimeLineRecouvrement from "@/components/TimeLineRecouvrement";
 import LegalModes from "@/components/LegalModes";
 import AvantagesAvocat from "@/components/AvantagesAvocat";
-import AccueilActions from "@/components/AccueilActions";
 import MiseEnDemeureGenerator from "@/components/MiseEnDemeureGenerator";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-gray-50 min-h-screen flex flex-col">
+    <div>
       <Navbar />
-      <section id="hero"><HeroSection /></section>
-      <section id="timeline" className="my-10"><TimelineRecouvrement /></section>
-      <section id="legalmodes" className="my-10"><LegalModes /></section>
-      <section id="avantagesavocat" className="my-10"><AvantagesAvocat /></section>
-      <section id="accueilactions" className="my-10"><AccueilActions /></section>
-      <section id="generatedoc" className="my-10"><MiseEnDemeureGenerator /></section>
+      <HeroSection />
+      <AccueilActions />
+      <TimeLineRecouvrement />
+      <LegalModes />
+      <AvantagesAvocat />
+      <MiseEnDemeureGenerator />
       <Footer />
     </div>
   );
